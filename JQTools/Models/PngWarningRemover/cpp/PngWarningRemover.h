b3c53fc1d9f3e7847b24072604349e4fe00b971e
Jason <188080501@qq.com>
2016-05-15 15:06:54 +0800

    Upload V16.5.15

---
+++ JQTools/Models/PngWarningRemover/cpp/PngWarningRemover.h
@@ -0,0 +1,31 @@
+﻿#ifndef __PngWarningRemover_h__
+#define __PngWarningRemover_h__
+
+// Qt lib import
+#include <QObject>
+
+#define PNGWARNINGREMOVERINITIALIZA                                                                     \
+{                                                                                                       \
+    qmlRegisterType<PngWarningRemover::Manage>("PngWarningRemover", 1, 0, "PngWarningRemoverManage");   \
+}
+
+namespace PngWarningRemover
+{
+
+class Manage: public QObject
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(Manage)
+
+public:
+    Manage();
+
+    ~Manage();
+
+public slots:
+    void conversationPng();
+};
+
+}
+
+#endif//__PngWarningRemover_h__
